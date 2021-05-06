1edb881f86e98574425855ac061b02c5d6e02f32
yukikohiga-sys <75060610+yukikohiga-sys@users.noreply.github.com>
2021-05-06 12:43:35 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,37 @@
-let img;
+// 花のイメージ用変数
+let blueFlowerImage, yellowFlowerImage, orangeFlowerImage;
+let flowerImage;
 
+// ３つの花の画像ファイルを読み込む
 function preload() {
-  img = loadImage('animal1.png');
+    blueFlowerImage = loadImage('images/blue.png');
+    yellowFlowerImage = loadImage('images/yellow.png');
+    orangeFlowerImage = loadImage('images/orange.png');
 }
 
 function setup() {
- createCanvas(400, 400);
- background(0);
- 
-}
-function draw() {
-  image(img,0,0,400,400);
+    createCanvas(400, 300);
+    background(230);
+    // 6行3列
+    for (let x = 0; x < 6; x++) {
+        // %演算子と3を使って、0,1,2,0,1,2という循環を得る
+        // 0と3列めは青い花
+        if (x % 3 === 0) {
+            flowerImage = blueFlowerImage;
+            // 1と4列めは黄色い花
+        }
+        else if (x % 3 === 1) {
+            flowerImage = yellowFlowerImage;
+            // 2と5列めはオレンジの花
+        }
+        else if (x % 3 === 2) {
+            flowerImage = orangeFlowerImage;
+        }
+
+        // xとyを使ってイメージを描く位置を決める
+        // 30はイメージの幅とイメージ間の空きを考慮した決め打ちの数値
+        for (let y = 0; y < 3; y++) {
+            image(flowerImage, x * 30, y * 30);
+        }
+    }
 }
